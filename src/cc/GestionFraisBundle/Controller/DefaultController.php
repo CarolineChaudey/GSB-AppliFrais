954b462a569c36d6a8149cad576d7af45b4fe60b
eleve <eleve@slam4sf2.sio.lla.fr>
2015-12-10 14:48:16 +0100

    formulaire

--- src/cc/GestionFraisBundle/Controller/DefaultController.php
+++ src/cc/GestionFraisBundle/Controller/DefaultController.php
@@ -3,11 +3,38 @@
 namespace cc\GestionFraisBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction(Request $request)
     {
-        return $this->render('ccGestionFraisBundle:Default:index.html.twig', array('name' => $name));
+        $form = $this->createFormBuilder()
+                ->add('login', 'text')
+                ->add('mdp', 'password')
+                ->add('profil', 'choice', array('choices'=>array('visiteur'=>'Visiteur',
+                                                            'comptable'=>'Comptable')))
+                ->add('valider', 'submit')
+                ->add('annuler', 'reset')
+                ->getForm();
+        
+        $form->handleRequest($request);
+        if($form->isValid()){
+            $login = $form->getData('login');
+            $mdp = $form->getData('mdp');
+            $profil = $form->getData('profil');
+            $infosCo = array(
+                'login'=>$login,
+                'mdp'=>$mdp,
+                'profil'=>$profil
+            );
+            
+            return $this->render('ccGestionFraisBundle:Default:test.html.twig', array('login'=>$login,
+                                                                                        'mdp'=>$mdp,
+                                                                                        'profil'=>$profil));
+        }
+        
+        return $this->render('ccGestionFraisBundle:Default:v_connexion.html.twig', array('form'=>$form->createView()));
     }
 }
