bee2b7cb8ddeed5e5618c7212536ffb8a6cd42f8
caroline <caroline@caroline-Satellite-L670>
2016-01-09 14:20:06 +0100

    création Modele

--- src/cc/GestionFraisBundle/Controller/VisiteurController.php
+++ src/cc/GestionFraisBundle/Controller/VisiteurController.php
@@ -12,4 +12,10 @@ class VisiteurController extends Controller
     public function indexAction(Request $request){
         return $this->render('ccGestionFraisBundle:Visiteur:v_sommaire.html.twig', array("user" => $request->getSession()->get("user")));
     }
+    
+    public function saisieAction(Request $request){
+        $modele = $this->container->get('modele');
+        // à développer
+        return $this->render('ccGestionFraisBundle:Visiteur:v_saisie.html.twig', array("user" => $request->getSession()->get("user")));
+    }
 }
